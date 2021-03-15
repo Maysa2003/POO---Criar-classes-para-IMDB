96eee48419280d1aaaf1efe47dc2bc91f1dbbd5f
Maysa Lima <80075101+Maysa2003@users.noreply.github.com>
2021-03-15 15:35:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,64 @@
-var input = require('fs').readFileSync('/dev/stdin', 'utf8');
-var lines = input.split('\n');
-const fs = require ('fs');
-const readline = require('readline');
+/* Lógico_4: */
 
-let inputNomes = fs.readFileSync("./name/data.tsv", "utf8");
-let nomes = inputNomes.split("\n");
-nomes.shift();
+CREATE TABLE Redes_sociais_ (
+    Id INTEGER PRIMARY KEY,
+    instagram_ VARCHAR,
+    facebook VARCHAR,
+    whatsApp VARCHAR
+);
 
-}
-async function lerAtores() {
-    const linhas = readline.createInterface({
-        input: fs.createReadStream('./dados/principals.tsv')
-    });
-    for await(let linha of linhas) {
-        let [tconst,ordering ,nconst, category, job, characters] = linha.split("\t");
-        if(filmes[tconst]) {
-            filmes[tconst].elenco = filmes[tconst].elenco || [];
-            filmes[tconst].elenco.push ( 
-                {
-                    idPessoa: nconst, 
-                    categoria: category,
-                    papel: job,
-                    personagens: characters.split(",") 
-                }
-            );
-        } 
-        let filmes = {};
-        let atores = Atores.filter ((elemento) => elemento.Filme);
-        for (let i = 0; let i < filmes.length; i++){
-          filmes.push([i]);
-          
-        }
-    }  
-}
+CREATE TABLE Influenciadores (
+    CPF INTEGER PRIMARY KEY,
+    nome BOOLEAN,
+    cidade VARCHAR,
+    telefone INTEGER,
+    divulgacao VARCHAR
+);
+
+CREATE TABLE midias (
+    Arquivo BOOLEAN PRIMARY KEY,
+    fotos VARCHAR,
+    videos VARCHAR,
+    links VARCHAR
+);
+
+CREATE TABLE comercio (
+    CNPJ INTEGER PRIMARY KEY,
+    estabeleciemento VARCHAR,
+    administrador VARCHAR
+);
+
+CREATE TABLE vendas (
+    Codigo_do_produto VARCHAR PRIMARY KEY,
+    marca_do_produto_ VARCHAR,
+    agrupamento_do_produto_ VARCHAR,
+    preco INTEGER
+);
+
+CREATE TABLE comunicacao (
+    Id INTEGER PRIMARY KEY,
+    chat VARCHAR,
+    telefone INTEGER,
+    story VARCHAR,
+    fk_vendas_Codigo_do_produto VARCHAR
+);
+
+CREATE TABLE possui_ (
+    fk_Redes_sociais__Id INTEGER,
+    fk_Influenciadores_CPF INTEGER
+);
+
+CREATE TABLE tem (
+    fk_Redes_sociais__Id INTEGER,
+    fk_midias_Arquivo BOOLEAN
+);
+
+CREATE TABLE colaboram (
+    fk_Influenciadores_CPF INTEGER,
+    fk_comercio_CNPJ INTEGER
+);
+
+CREATE TABLE ajudam (
+    fk_comercio_CNPJ INTEGER,
+    fk_vendas_Codigo_do_produto VARCHAR
+);
